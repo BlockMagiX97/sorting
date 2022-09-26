204a2aa6a5d27f1e197f68c703438fd0b4716e2e
Maxmilian Mázek <blockmagix97@gmail.com>
2022-09-26 19:14:50 +0200

    made made_same_lenght

--- sorting.py
+++ sorting.py
@@ -1,10 +1,28 @@
+def make_same_lenght(list_val):
+    max = 0
+    for i in list_val:
+        lenght = len(str(i))
+        if lenght > max:
+            max = lenght
+    
+    for i in enumerate(list_val):
+        val = str(list_val[i])
+        val = '0' * (max - len(val)) + val
+        list_val[i] = val
+    
+    return list_val
+        
+
+
 def regix(list_val):
     # makes counter list
     counter = {}
     for key in range(0 , 10):
         counter[key] = 0
+    
 
-    return(list_val[0][0])
+    
 
-print(regix([10]))
+
+        
     
